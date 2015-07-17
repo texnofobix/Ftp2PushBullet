fde0326b4c9d713ab2b244c633b3d8aa9822104b
Dustin S <texnofobix@users.noreply.github.com>
2015-07-16 23:57:32 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+from ftp2pushbullet import MyHandler
+
+#global api_key, user, password
+#api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # PushBullet API Key
+#user = 'user'                                # FTP User
+#password = 'password'                        # FTP Password
+
+
+    
